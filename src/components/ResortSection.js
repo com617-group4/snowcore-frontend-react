40c11dc9081693ba65a8ea10e233d9dc13066a36
uchitelq <stshk@abv.bg>
2022-05-08 19:42:12 +0100

    login system 1.0

--- src/components/ResortSection.js
+++ src/components/ResortSection.js
@@ -1,31 +1,55 @@
+import axios from "axios";
 import React from "react";
+import { useCallback, useContext } from "react/cjs/react.production.min";
 
 function ResortSection(props) {
-  console.log(props)
 
+  const fav=async()=>{
+    axios.get('http://localhost:8070/auth/favourite/'+props.resort._id,{
+        headers:{
+          Authorization:`Bearer ${props.user}`
+        }
+      }).then(function (response){
+        console.log(response)
+      }).catch(function (error) {
+        console.log( error.response.data.message)
+        //console.log(error.message);
+        alert(error.response.data.message)
+      });
+  }
+
+ // const msg=useContext(UserContext);
+
+  //console.log(msg)
+  //console.log(props.resort._id)
+  var snow=0
+  if(props.hasOwnProperty('resort.weather.snow1h')){
+    snow=props.resort.weather.snow1h
+  }
   return (
    
-    <div className="bg-blue-300 self-center h-fit  m-9 p-4">
-      <div className="first_row flex justify-around mb-3 bg-black text-white">
-        <div className="bg-white">
+    <div className="bg-blue-300 self-center h-fit  mb-10 p-4">
+      <div className="first_row flex justify-around mb-3 bg-white ">
+        <div>
           <h3 className="font-bold text-black">{props.resort.name}</h3>
         </div>
         <div className="temperature">
           <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clipRule="evenodd" />
           </svg>
-          <span className="inline-block"> {props.resort.weather.temp+"°C"} </span>
+         
+          <span className="inline-block">{props.resort.weather.description}, {props.resort.weather.temp+"°C"}  </span>
         </div>
 
         <div className="snow">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="inline-block bi bi-snow" viewBox="0 0 16 16">
             <path d="M8 16a.5.5 0 0 1-.5-.5v-1.293l-.646.647a.5.5 0 0 1-.707-.708L7.5 12.793V8.866l-3.4 1.963-.496 1.85a.5.5 0 1 1-.966-.26l.237-.882-1.12.646a.5.5 0 0 1-.5-.866l1.12-.646-.884-.237a.5.5 0 1 1 .26-.966l1.848.495L7 8 3.6 6.037l-1.85.495a.5.5 0 0 1-.258-.966l.883-.237-1.12-.646a.5.5 0 1 1 .5-.866l1.12.646-.237-.883a.5.5 0 1 1 .966-.258l.495 1.849L7.5 7.134V3.207L6.147 1.854a.5.5 0 1 1 .707-.708l.646.647V.5a.5.5 0 1 1 1 0v1.293l.647-.647a.5.5 0 1 1 .707.708L8.5 3.207v3.927l3.4-1.963.496-1.85a.5.5 0 1 1 .966.26l-.236.882 1.12-.646a.5.5 0 0 1 .5.866l-1.12.646.883.237a.5.5 0 1 1-.26.966l-1.848-.495L9 8l3.4 1.963 1.849-.495a.5.5 0 0 1 .259.966l-.883.237 1.12.646a.5.5 0 0 1-.5.866l-1.12-.646.236.883a.5.5 0 1 1-.966.258l-.495-1.849-3.4-1.963v3.927l1.353 1.353a.5.5 0 0 1-.707.708l-.647-.647V15.5a.5.5 0 0 1-.5.5z" />
           </svg>
-          <span className="inline-block"> 0 </span>
+          <span className="inline-block"> {snow} </span>
         </div>
 
         <div className="recommendations">
-          <svg xmlns="http://www.w3.org/2000/svg" className="inline-block h-6 w-6 hover:bg-yellow-400  cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <svg onClick={fav} xmlns="http://www.w3.org/2000/svg" className="inline-block h-6 w-6 hover:bg-yellow-400  cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
             <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
           </svg>
           <span className="inline-block"> {props.resort.favourites} </span>
@@ -45,7 +69,7 @@ function ResortSection(props) {
             <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
             <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
-          <span className="inline-block">Piste Map</span>
+          <a target="_blank" href="https://www.ski-school-courchevel1650.co.uk/public/files/Plans%20des%20Pistes/Plan-3Vallees-1819-BD-avec-legende.pdf" className="inline-block">Piste Map</a>
         </div>
 
         <div className="email-alerts-link cursor-pointer hover:bg-blue-500">
--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 import { render } from 'react-dom';
 import reactDom from 'react-dom';
 //CHANGE ASAP
-const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN0c2hrQGFidi5iZyIsImlhdCI6MTY0OTA2NzQ1MSwiZXhwIjoxNjQ5MDk2MjUxfQ.JJQ4--jlR53sUILx2HDiQeFZdm3JJ07PZ1Si7mYk4qg";
+var token="";
 const apiurl="http://localhost:8070";
 /* BAD PRACTICE.
 axios.interceptors.request.use(
@@ -19,13 +19,13 @@ axios.interceptors.request.use(
     return Promise.reject(error);
   }
 )*/
-const authAxios=axios.create({
+var authAxios=axios.create({
   baseURL:apiurl,
   headers:{
     Authorization:`Bearer ${token}`
   }
 })
-
+//var userToken=""
 
 function openMap(resorts){
     ReactDOM.render(
@@ -35,60 +35,184 @@ function openMap(resorts){
     , document.getElementById("response"))
 }
   
-  function openUser(resort){
-    ReactDOM.render( <div className="flex flex-col h-4/5 ">
-           <UserSection></UserSection>
 
-    {resort.map(( i) => {  
-      
-      return (<ResortSection
-       resort={i}
-      ></ResortSection>);
-    })
-  }</div>,document.getElementById("response"))
-
-
-   /* ReactDOM.render(
-      
-      <div className="flex flex-col h-4/5 ">
-
-     <UserSection></UserSection>
-     
-       <ResortSection
-        resorts={resorts}
-        />
-     </div>
-      , document.getElementById("response"));*/
-  }
-
+/*
   function openHome(resortsResponse){
     var res=Array();
     var toRender = "";
     resortsResponse.then((resorts) => {
       resorts.data.map(async (resort) => 
       {
-      
-        
         console.log(resort.name)
        ReactDOM.render(
           <ResortSection
           resortsParsed={resort}
+
           ></ResortSection>
          , document.getElementById("response"))
          
       });
     });
   
-   }
+   }*/
   
 function Header(){
   const [resorts,setResorts]=useState([]);
+  const [favresorts,setFavResorts]=useState([]);
+
   const [requestError,setRequestError]=useState();
+  const [loginstate,setLoginState]= useState();
+  const [fname,setFName]=useState();
+  const [lname,setLName]=useState();
+  const [pass,setPass]=useState();
+  const [cpass,setCPass]=useState();
+  const [mail,setMail]=useState();
+  const [loginmail,setLoginMail]=useState();
+  const [loginpass,setLoginPass]=useState();
+  const test=()=>{
+    console.log(fname+" "+lname+" "+pass+" "+cpass+" "+mail)
+  }
+  
+
+  const getFaved =useCallback(async()=>{
+    if(token==""){
+      alert("Please Log In or register to access this feature")
+    }
+    else{
+  
+    //console.log(favresorts)
+    //setFavResorts([])
+    //console.log(favresorts)
+
+    axios.get('http://localhost:8070/auth/favourites', {
+        headers: {
+          Authorization:`Bearer ${token}`
+        }
+      })
+      .then(function(response){
+          console.log(response)
+          //setFavResorts(response)
+          ReactDOM.render( <div className="flex flex-col h-4/5 ">
+          <UserSection></UserSection>
+
+          {response.data.map(( i) => {  
+            
+            return (<ResortSection
+            resort={i}
+            user={token}
+            ></ResortSection>);
+          })
+          }</div>,document.getElementById("response"))
+      })
+      .catch(function (error) {
+        console.log( error)
+        //console.log(error.message);
+        alert(error)
+      });
+ 
+    }
+  })
+
+
+
+
+
+  /*const openUser= function(resort){
+    if(token==""){
+      alert("Please Log In or register to access this feature")
+    }
+    else{
+      getFaved()
+      ReactDOM.render( <div className="flex flex-col h-4/5 ">
+            <UserSection></UserSection>
+
+      {resort.map(( i) => {  
+        
+        return (<ResortSection
+        resort={i}
+        user={token}
+        ></ResortSection>);
+      })
+    }</div>,document.getElementById("response"))
+    }
+  }*/
+//d31d924747487f3c6f202c18c79daa288ac8ec2b86cf618e133d9d9c05862adb
+  const login =useCallback(async()=>{
+
+      axios.post('http://localhost:8070/auth/signin',{
+        
+        email: loginmail,
+        password: loginpass
+      },{
+        headers:{
+          Authorization:`Bearer ${token}`
+        }
+      }
+      ).then(function (response) {
+        console.log(response)
+        console.log(token)
+        setLoginState(response.data.accessToken)
+        token=response.data.accessToken
+
+        fetchResorts()
+        ReactDOM.render(
+          <p>Hello {response.data.name} {response.data.surname}</p>
+         , document.getElementById("welcome_txt"))
+         //getFaved()
+       
+      })
+        .catch(function (error) {
+          console.log( error.response.data.message)
+          //console.log(error.message);
+          alert(error.response.data.message)
+        });
+  })
+
+  const signup=useCallback(async()=>{
+    if(pass==cpass){
+      console.log(fname+" "+lname+" "+pass+" "+cpass+" "+mail)
+      try{
+    
+      axios.post('http://localhost:8070/auth/signup', {
+          email: mail,
+          name: fname,
+          surname: lname,
+          password: pass
+        })
+        .then(function (response) {
+
+            setLoginState(response.data.activationToken)//not working???
+            console.log(response)
+            token=response.data.activationToken
+            console.log(token)
+
+            alert("Successfully Registered!")
+            fetchResorts()
+            ReactDOM.render(
+              <p>Hello {fname} {lname}</p>
+             , document.getElementById("welcome_txt"))
+           
+         
+         
+        })
+        .catch(function (error) {
+          console.log( error.response.data.message)
+          //console.log(error.message);
+          alert(error.response.data.message)
+        });
+      return loginstate;        
+    }
+    catch(e){
+      setRequestError(e.message);
+    }
+    }
+    else{
+      alert("Passwords do not match!")
+    }
+});
 
   const fetchResorts=useCallback(async()=>{
-   
-
-    try{
+       try{
       
         const res=await authAxios.get(`http://localhost:8070/resort/`).then((resortsret) => {
         
@@ -96,24 +220,20 @@ function Header(){
             return resortsret.data
 
         }).then((resortsretdata)=>{
-          ReactDOM.render(<div className="container mt-6 flex-col gap-4">
+          ReactDOM.render(<div className=" p-2 mt-6 flex-col gap-4">
             {resortsretdata.map(( i) => {     
-              return (<ResortSection
+              return (
+              
+              <ResortSection
+                key={i._id}
                resort={i}
+               user={token}
               ></ResortSection>);
             })
           }</div>,document.getElementById("response"))
         })
-          
-          
-    
-        return res;
-          
-          // expected output: "Success!"
-        
-        
+        return res;        
     }
-    
     catch(e){
       setRequestError(e.message);
     }
@@ -135,15 +255,17 @@ function Header(){
           <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
           </svg>
         </button>
-       
-        <button onClick={function(){  openUser(resorts)}} className="rounded-full mt-1 bg-white ">
+        <div >
+        <button onClick={function(){  getFaved()}} className="rounded-full mt-1 bg-white ">
             <svg xmlns="http://www.w3.org/2000/svg" width="55px" height="55px" fill="currentColor"  viewBox="0 0 16 16">
           <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
           </svg>
           </button>
-      
+          <div id='welcome_txt'></div>
+          </div>
     </div>
-    <div id="response" className='bg-fixed bg-cover bg-center' style={{ 
+    
+    <div id="response" className='flex flex-col bg-fixed bg-cover bg-center mt-12' style={{ 
       backgroundImage: `url("../src/img/resort_bckg.jpg")` }} >
       
       <div className="flex flex-col">
@@ -154,18 +276,20 @@ function Header(){
 
         <div className='flex flex-col w-1/2 mt-5' >
           <h3><b>LOGIN</b></h3>
-          <input className="border-b-4 border-blue-400 mb-4 mt-2 focus:border-red-600 focus:outline-none" type="text" id="fname" name="fname" placeholder="Username"/>
-          <input className="border-b-4 border-blue-400 mb-4 focus:border-red-600 focus:outline-none" type="password" id="lname" name="lname" placeholder="Password"/>
-          <button className="hover:bg-red-600 hover:cursor-pointer border-2 border-blue-50 text-white bg-blue-400 w-20 p-1" value="Submit">Login</button>
+          <input value={loginmail} onChange={(e)=>setLoginMail(e.target.value)} required className="border-b-4 border-blue-400 mb-4 mt-2 focus:border-red-600 focus:outline-none" type="email" placeholder="Email"/>
+          <input value={loginpass} onChange={(e)=>setLoginPass(e.target.value)} required className="border-b-4 border-blue-400 mb-4 focus:border-red-600 focus:outline-none" type="password" id="lname" name="lname" placeholder="Password"/>
+          <button  onClick={ function(){  login()}} className="hover:bg-red-600 hover:cursor-pointer border-2 border-blue-50 text-white bg-blue-400 w-20 p-1" value="Submit">Login</button>
         </div>
 
         <div className='flex flex-col w-1/2 mt-5' >
           <h3><b>No Account? Register here.</b></h3>
-          <input className="border-b-4 border-blue-400 mb-4 mt-2 focus:border-red-600 focus:outline-none" type="text" id="fname" name="fname" placeholder="Username"/>
-          <input className="border-b-4 border-blue-400 mb-4 focus:border-red-600 focus:outline-none" type="password" id="lname" name="lname" placeholder="Password"/>
-          <input className="border-b-4 border-blue-400 mb-4 focus:border-red-600 focus:outline-none" type="password" id="lname" name="lname" placeholder="Confirm Password"/>
+          <input value={fname} onChange={(e)=>setFName(e.target.value)} required className="border-b-4 border-blue-400 mb-4 mt-2 focus:border-red-600 focus:outline-none"  type="text" id="fname" name="fname" placeholder="First Name"/>
+          <input value={lname} onChange={(e)=>setLName(e.target.value)} required className="border-b-4 border-blue-400 mb-4 mt-2 focus:border-red-600 focus:outline-none" type="text" id="lname" name="lname" placeholder="Last Name"/>
+          <input value={mail} onChange={(e)=>setMail(e.target.value)} required className="border-b-4 border-blue-400 mb-4 focus:border-red-600 focus:outline-none" type="email" id="mail" name="mail" placeholder="E-Mail"/>
+          <input value={pass} onChange={(e)=>setPass(e.target.value)} required className="border-b-4 border-blue-400 mb-4 focus:border-red-600 focus:outline-none" type="password" id="pass" name="pass" placeholder="Password"/>
+          <input value={cpass} onChange={(e)=>setCPass(e.target.value)} required className="border-b-4 border-blue-400 mb-4 focus:border-red-600 focus:outline-none" type="password" id="cpass" name="cpass" placeholder="Confirm Password"/>
 
-          <button className="hover:bg-red-600 hover:cursor-pointer border-2 border-blue-50 text-white bg-blue-400 w-20 p-1" value="Submit">Register</button>
+          <button  onClick={function(){  signup()}} className="hover:bg-red-600 hover:cursor-pointer border-2 border-blue-50 text-white bg-blue-400 w-20 p-1" value="Submit">Register</button>
         </div>
         </div>
       </div>
